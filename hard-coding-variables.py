cd3e9cb033dd4bf8eae569c79dfe2da4aca2fa78
kpranavii <pkantred@gitam.in>
2024-07-22 04:02:48 +0530

    Update hard-coding-variables.py

--- hard-coding-variables.py
+++ hard-coding-variables.py
@@ -1,25 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def quadratic_sol(t):
-  #assuming coefficients
-  a = 0.1
-  b = -1
-  c = 10
+# to calculate precipitation
+def calculate_precipitation(temperature, humidity,pressure):
+    # Coefficients
+    a = 0.01 
+    b = -0.6
+    c = 0.2
+    
+    precipitation = a * humidity**2 + b * temperature + c* pressure
+    return max(0, precipitation)  # non negative value for precipitation
 
-  temperature = a*t**2 + b*t + c
-  return temperature
+humidity_range = np.linspace(0, 100, 100)  # Humidity range from 0% to 100%
+temperature = 21.0  # temperature value
+pressure= 600.0 #pressure value
 
-def main():
-  time = np.linspace(0,72)
-  precipitation= quadratic_sol(time)
-  plt.plot(time, precipitation, label='Hard-coded variables')
-  plt.xlabel('Time(in hour)')
-  plt.ylabel('Precipitation(in mm)')
-  plt.legend()
-  plt.title('Weather Modeling with quadratic solution (hard-coded variables)')
+# Calculate precipitation for each humidity in the range
+precipitation_values = [calculate_precipitation(temperature, hum,pressure) for hum in humidity_range]
 
-  plt.show()
-
-if __name__ == '__main__':
-  main()
+plt.plot(humidity_range, precipitation_values)
+plt.xlabel('Humidity (%)')
+plt.ylabel('Precipitation (mm)')
+plt.title('Relationship between Humidity and Precipitation')
+plt.grid(True)
+plt.legend()
+plt.show()
