7fff0364a35e0d86f18a5c4e05704ecfd0b73e54
kpranavii <pkantred@gitam.in>
2024-07-22 04:19:40 +0530

    updated input-variables

--- input-variables.py
+++ input-variables.py
@@ -12,8 +12,10 @@ def calculate_precipitation(temperature, humidity,pressure):
     return max(0, precipitation)  # non negative value for precipitation
 
 humidity_range = np.linspace(0, 100, 100)  # Humidity range from 0% to 100%
-temperature = float(input("Enter temperature value"))
-pressure= float(input("Enter pressure value"))
+temperature = float(input("Enter temperature value(in ℃)"))
+pressure= float(input("Enter pressure value(in mmHg)"))
+humidity=float(input("Enter humidity value(in %)"))
+print("Precipitation: ",calculate_precipitation(temperature, humidity,pressure),"mm")
 
 # Calculate precipitation for each humidity in the range
 precipitation_values = [calculate_precipitation(temperature, hum,pressure) for hum in humidity_range]
@@ -23,5 +25,4 @@ plt.xlabel('Humidity (%)')
 plt.ylabel('Precipitation (mm)')
 plt.title('Relationship between Humidity and Precipitation')
 plt.grid(True)
-plt.legend()
 plt.show()
