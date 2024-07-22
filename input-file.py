ce845cc411010577b5ce1b4d6b73a10807e08af8
kpranavii <pkantred@gitam.in>
2024-07-22 13:59:50 +0530

    update

--- input-file.py
+++ input-file.py
@@ -10,3 +10,22 @@ def calculate_precipitation(temperature, humidity,pressure):
     
     precipitation = a * humidity**2 + b * temperature + c
     return max(0, precipitation)  # non negative value for precipitation
+
+df= pd.read_csv('data.csv')
+temperature=[]
+humidity=[]
+pressure=[]
+for index, row in df.iterrows():
+    temperature.append(row['Temperature'])
+    humidity.append(row['Humidity'])
+    pressure.append(row['Pressure'])
+precipitation_values=[]
+for i in range(0,6):
+    precipitation_values.append(calculate_precipitation(temperature[i], humidity[i],pressure[i]))
+
+plt.plot(humidity, precipitation_values)
+plt.xlabel('Humidity (%)')
+plt.ylabel('Precipitation (mm)')
+plt.title('Relationship between Humidity and Precipitation')
+plt.grid(True)
+plt.show()
